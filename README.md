2dccc55d59ee8fefbe5de037fabc77f169539e06
David Egan <david@carawebs.com>
2019-10-07 21:47:22 +0100

    Add readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+Solve a System of Two Congruences
+=================================
+This programme uses the Chinese Remainder Theorem to solve a system of two linear congruences.
+
+Note that the moduli must be coprime.
--- compute.py
+++ compute.py
@@ -3,23 +3,22 @@
 from compute_inverse import *
 
 def ChineseRemainderTheorem(n1, r1, n2, r2):
-    # Are n1 and n2 coprime?
-
-    x1 = r1*n2*compute_inverse(n2, n1)
-    x2 = r2*n1*compute_inverse(n1, n2)
+    x1 = r1 * n2 * compute_inverse(n2, n1)
+    x2 = r2 * n1 * compute_inverse(n1, n2)
     x = x1 + x2
-#    while (x > (n1 * n2)):
-#        x -= n1 * n2
     return x % (n1 * n2)
 
 
 def main():
-    n1 = int(input("Enter n1: "))
+    print("This programme solves a system of congruences for x such that:")
+    print("x ≡ r1 (mod a) and x ≡ r2 (mod b)")
     r1 = int(input("Enter r1: "))
-    n2 = int(input("Enter n2: "))
+    mod1 = int(input("Enter first modulo (a): "))
     r2 = int(input("Enter r2: "))
-    result = ChineseRemainderTheorem(n1, r1, n2, r2)
-    print(result)
+    mod2 = int(input("Enter second modulo (b): "))
+    print("x ≡ {} (mod {}) and x ≡ {} (mod {})".format(r1, mod1, r2, mod2))
+    result = ChineseRemainderTheorem(mod1, r1, mod2, r2)
+    print("The smallest value that satisfies the inital conditions for x is {}".format(result))
 
 if __name__ == '__main__':
     main()
